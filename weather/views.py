e11ea573a207fe00d9bd9d87dc0b88a567b52201
manhnguyen12342 <manhvui123321@gmail.com>
2024-05-02 10:15:44 +0700

    change code

--- weather/views.py
+++ weather/views.py
@@ -22,7 +22,7 @@ class WeatherAPIView(APIView):
         
 def post(self, request):
         serializer = WeatherDataSerializer(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
