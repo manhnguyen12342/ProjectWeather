f3152fa6671f97457db196e007787474637d7faf
manhnguyen12342 <manhvui123321@gmail.com>
2024-05-06 09:25:42 +0700

    not done code 2

--- weather/views.py
+++ weather/views.py
@@ -7,30 +7,28 @@ from django.conf import settings
 import const
 
 class WeatherAPIView(APIView):
-        def get(self, request):
+    def get(self, request):
             
-            api_key = settings.API_KEYS
-            location = const.Hanoi_Location
-            url = settings.WEATHER_API_URL.format(city=location,api_key=api_key)
+        api_key = settings.API_KEYS
+        location = const.Hanoi_Location
+        url = settings.WEATHER_API_URL.format(city=location,api_key=api_key)
             
-            response = requests.get(url)
-            data =response.json()
-            ans={
-                "location":data["name"],
-                "temperature":data["main"]["temp"],
-                "humidity":data["main"]["humidity"],
-                
+        response = requests.get(url)
+        data =  response.json()
+        ans = {
+            "location":data["name"],
+            "temperature":data["main"]["temp"],
+            "humidity":data["main"]["humidity"],
             }
-            serializer = WeatherDataSerializer(data=ans)
-            serializer.is_valid(raise_exception=True)
-            return Response(serializer.data)
+        serializer = WeatherDataSerializer(data=ans)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data)
         
-        def post(self, request):
-            
-            serializer = WeatherDataSerializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    def post(self, request):
+        serializer = WeatherDataSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class Atherods(APIView):
     def post(self, request):
